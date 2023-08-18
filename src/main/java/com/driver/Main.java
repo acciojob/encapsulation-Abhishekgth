97fb14b0979ff80639a33939041d4aa652bc0e2d
Abhishek Mumar Mishra <mishraabhiak@gmail.com>
2023-08-18 14:11:38 +0530

    solve

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly r=new RWOnly();
+       // r.name="abhi";//java: name has private access in com.driver.RWOnly
+        r.setName("Abhishek");
+        System.out.println(r.getName());
+    }
   
 }
